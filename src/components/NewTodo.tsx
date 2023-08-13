3bef2491e7448689c3badc14c41041e441f41fb6
Ahmed Basuony <99911074+a-basuony@users.noreply.github.com>
2023-08-13 10:24:13 +0300

    update

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const NewTodo: React.FC = () => {
+  const inputTextRef = useRef<HTMLInputElement>(null);
+
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+
+    const enteredInputText = inputTextRef.current?.value;
+
+    if (enteredInputText?.trim().length === 0) {
+      //throw new Error
+      return;
+    }
   };
 
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="input">add Item</label>
-      <input type="text" placeholder="Add a new todo..." id="input" />
+      <input
+        type="text"
+        placeholder="Add a new todo..."
+        id="input"
+        ref={inputTextRef}
+      />
       <button>add</button>
     </form>
   );
